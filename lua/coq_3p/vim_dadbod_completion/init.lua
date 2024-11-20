6d984d9c8e54439da7da85334e3ebc5cb7436eff
Herz3h <cresuso@gmail.com>
2024-11-20 11:14:50 +0100

    Add mysql filetype to vim_dadbod_completion

--- lua/coq_3p/vim_dadbod_completion/init.lua
+++ lua/coq_3p/vim_dadbod_completion/init.lua
@@ -2,6 +2,6 @@ return function(spec)
   return require("coq_3p.omnifunc") {
     use_cache = true,
     omnifunc = "vim_dadbod_completion#omni",
-    filetypes = {"sql", "psql"}
+    filetypes = {"mysql", "sql", "psql"}
   }
 end
